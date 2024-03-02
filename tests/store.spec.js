7814d6a0af4a83782cb6d05136b6d2255626afc7
Lee <leesjensen@gmail.com>
2024-03-02 08:28:44 -0700

    fix(uitesting) to work

--- tests/store.spec.js
+++ tests/store.spec.js
@@ -1,34 +1,35 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('testAddStoreButton', async ({ page }) => {
-  const serverStoreJson = [
+test.beforeEach(async ({ page }) => {
+  let serverStoreJson = [
     { name: 'nyc', date: '2028-01-01' },
     { name: 'san diego', date: '2032-10-31' },
   ];
 
   await page.route('*/**/api/store', async (route) => {
     expect(route.request().method()).toBe('GET');
-    await route.fulfill({ store: serverStoreJson });
+    await route.fulfill({ json: { store: serverStoreJson } });
   });
 
   await page.route('*/**/api/store/provo', async (route) => {
     expect(route.request().method()).toBe('POST');
-    const json = [
-      { name: 'nyc', date: '2028-01-01' },
-      { name: 'san diego', date: '2032-10-31' },
+    serverStoreJson = [
+      ...serverStoreJson,
+      { name: 'provo', date: '2021-10-31' },
     ];
-    await route.fulfill({ json });
+    await route.fulfill({ json: { store: serverStoreJson } });
   });
 
   await page.goto('http://localhost:5173/');
+});
 
+test('testAddStoreButton', async ({ page }) => {
   await expect(page).toHaveTitle('DevOps Demo');
 
   await page.locator('css=input').fill('provo');
 
   const addStoreBtn = page.getByRole('button', { name: 'Add' });
   await addStoreBtn.click();
-  const storeTable = page.locator('css=table td:first-child');
-  await expect(storeTable).toHaveText('provo');
+  await expect(page.getByRole('cell', { name: 'provo' })).toHaveText('provo');
 });
