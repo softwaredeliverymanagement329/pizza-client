6f296ab61e9f362b2b2ccdc78bb8009ec04a663b
Lee <lee.jensen@agilix.com>
2024-03-06 14:45:13 -0700

    enhance(store) use local storage

--- storeService.js
+++ storeService.js
@@ -8,7 +8,7 @@ class StoreService {
     if (this.useNetwork) {
       this.fetchRequest(callback, 'GET', '/api/store');
     } else {
-      callback(this.localStores);
+      callback(this.readLocalStores());
     }
   }
 
@@ -16,9 +16,20 @@ class StoreService {
     if (this.useNetwork) {
       this.fetchRequest(callback, 'POST', `/api/store/${storeName}`);
     } else {
-      this.localStores.store.push({ name: storeName, date: new Date().toLocaleString() });
-      callback(this.localStores);
+      const newStore = { name: storeName, date: new Date().toLocaleString() };
+      callback(this.writeLocalStore(newStore));
     }
+  }
+
+  readLocalStores() {
+    const store = localStorage.getItem('store') || `{"store": []}`;
+    return JSON.parse(store);
+  }
+
+  writeLocalStore(newStore) {
+    const stores = { store: [...this.readLocalStores().store, newStore] };
+    localStorage.setItem('store', JSON.stringify(stores));
+    return stores;
   }
 
   async fetchRequest(callback, method, path, headers = {}, body = null) {
